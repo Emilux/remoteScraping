efcc99c08ed1c6df897b36d62bb8360963786901
Emilien FUCHS <emilien.fuchs@gmail.com>
2021-08-17 09:41:24 +0200

    Add project files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import { MDBInput } from 'mdb-react-ui-kit';
+const { app  } = window.require('@electron/remote')
+const scrape = window.require('website-scraper');
+const SaveToExistingDirectoryPlugin = window.require('website-scraper-existing-directory');
 
 function App() {
+
+    const [link,setLink] = useState(null)
+
+    const submitSearchHandle = (e) => {
+        e.preventDefault();
+        setLink(document.getElementById('form1').value)
+        if (link){
+            const options = {
+                urls: [link],
+                directory: app.getPath('temp')+'/webpage/',
+                plugins: [ new SaveToExistingDirectoryPlugin() ]
+            };
+            scrape(options).then((result) => {console.log(result)});
+        }
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'app'}>
+        <div className={'p-4 mb-4 d-flex justify-content-center align-items-center flex-column'}>
+            <form onSubmit={submitSearchHandle} className="input-group">
+                <MDBInput label='Rechercher' id='form1' type='text' />
+                <button type="submit" className="btn btn-primary">
+                    <i className="fas fa-search"></i>
+                </button>
+            </form>
+        </div>
     </div>
   );
 }
